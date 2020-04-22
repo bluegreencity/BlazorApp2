6a1ea55e8b6e3137ecbfc2d1109518ad5ad39d75
Hyun-woo Shin <syndy123@naver.com>
2020-04-22 17:02:51 +0900

    2020.04.22

---
+++ BlazorApp2/Pages/PetItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Pages
+{
+    public class PetItem
+    {
+        public string PetId { get; set; }
+        public string Name { get; set; }
+    }
+}
